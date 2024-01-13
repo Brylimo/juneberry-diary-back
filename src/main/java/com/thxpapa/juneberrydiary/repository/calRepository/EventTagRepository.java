cc82b8c2a4b7aa55a8f0fc2de592a5bfb06d6cd2
brylimo <icj0103@gmail.com>
2024-01-13 23:59:51 +0900

    event tag service 수정

--- src/main/java/com/thxpapa/juneberrydiary/repository/calRepository/EventTagRepository.java
+++ src/main/java/com/thxpapa/juneberrydiary/repository/calRepository/EventTagRepository.java
@@ -1,7 +1,16 @@
 package com.thxpapa.juneberrydiary.repository.calRepository;
 
 import com.thxpapa.juneberrydiary.domain.cal.EventTag;
+import com.thxpapa.juneberrydiary.domain.cal.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface EventTagRepository extends JpaRepository<EventTag, Integer> {
+    @Query("SELECT t FROM Task t JOIN FETCH t.day WHERE t.eventCd= :eventCd AND t.day.date BETWEEN :startDate AND :endDate ORDER BY t.modDt ASC")
+    List<Task> findAllEventTagsByDate(@Param("eventCd") String eventCd, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
 }
--- src/main/java/com/thxpapa/juneberrydiary/service/cal/EventTagService.java
+++ src/main/java/com/thxpapa/juneberrydiary/service/cal/EventTagService.java
@@ -1,5 +1,10 @@
 package com.thxpapa.juneberrydiary.service.cal;
 
-public interface EventTagService {
+import com.thxpapa.juneberrydiary.domain.cal.EventTag;
 
+import java.time.LocalDate;
+import java.util.List;
+
+public interface EventTagService {
+    List<EventTag> getEventTagsByMonth(LocalDate startDate, LocalDate endDate);
 }
