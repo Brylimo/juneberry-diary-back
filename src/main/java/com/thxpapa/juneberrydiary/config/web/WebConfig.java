a941529ff0a24619005c2f0a9a04a623409465a0
brylimo <icj0103@gmail.com>
2024-02-10 23:52:25 +0900

    cors 수정 롤백(cafe24)

--- src/main/java/com/thxpapa/juneberrydiary/config/web/WebConfig.java
+++ src/main/java/com/thxpapa/juneberrydiary/config/web/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://juneberry-diary.vercel.app")
+                .allowedOrigins("https://juneberry-diary.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
